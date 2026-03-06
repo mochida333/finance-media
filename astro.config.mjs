0f841b8c9de1872e4e92075e4a034da25ea63fde
mochida <>
2026-03-06 18:03:03 +0900

    no message

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-const site = process.env.SITE_URL || 'https://example.com';
+const site = process.env.SITE_URL || 'https://finance-media.pages.dev/';
 
 // https://astro.build/config
 export default defineConfig({
